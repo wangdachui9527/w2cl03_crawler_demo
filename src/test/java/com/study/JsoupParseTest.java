86aad999a6f61e8b2cd807134aeb647a233c434f
wei_jun9158 <jj67967903>
2020-01-19 12:34:28 +0800

    1、根据id、class、attribute来获取元素

--- src/test/java/com/study/JsoupParseTest.java
+++ src/test/java/com/study/JsoupParseTest.java
@@ -3,6 +3,7 @@ package com.study;
 import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 
@@ -51,5 +52,26 @@ public class JsoupParseTest {
         Document content = Jsoup.parse(new File("C:\\Users\\admin123\\Desktop\\test.html"), "utf8");
         String title = content.getElementsByTag("title").first().text();
         System.out.println(title);
+    }
+
+
+    @Test
+    public void JsoupParseByDom() throws IOException {
+        Document document = Jsoup.parse(new File("C:\\Users\\admin123\\Desktop\\test.html"), "utf8");
+        //根据id获取元素
+//        Element fourL = document.getElementById("fourL");
+//        String text = fourL.text();
+//        String[] infos = text.split("查看详情");
+//        for (String info : infos) {
+//            System.out.println(info);
+//        }
+        //根据class获取元素
+//        Elements elementsByClass = document.getElementsByClass("four-txt");
+//        String text1 = elementsByClass.first().text();
+//        System.out.println(text1);
+
+        //根据属性的key和value来获取元素
+        Elements href = document.getElementsByAttributeValue("href", "/view/id/171081");
+        System.out.println(href.first().text());
     }
 }
