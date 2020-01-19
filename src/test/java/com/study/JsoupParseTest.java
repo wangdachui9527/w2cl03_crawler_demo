9aa309ed7581521454ee973f4839ec38dea1d7a2
wei_jun9158 <jj67967903>
2020-01-19 11:16:02 +0800

    1、使用Jsoup解析String字符串中的数据

--- src/test/java/com/study/JsoupParseTest.java
+++ src/test/java/com/study/JsoupParseTest.java
@@ -1,10 +1,13 @@
 package com.study;
 
+import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -14,11 +17,27 @@ import java.net.URL;
  * @Date 2020/1/19 10:41
  */
 public class JsoupParseTest {
+    /**
+     * 使用Jsoup解析网址Url上的数据
+     * @throws Exception
+     */
     @Test
     public void jsoupParseTest() throws Exception {
         Document doc = Jsoup.parse(new URL("https://szsg.2144.cn"), 1000);
         Elements titles = doc.getElementsByTag("title");
         String text = titles.first().text();
         System.out.println(text);
+    }
+
+    /**
+     * 使用Jsoup解析String字符串中的数据
+     * @throws Exception
+     */
+    @Test
+    public void jsoupParseStringTest() throws Exception {
+        String content = FileUtils.readFileToString(new File("C:\\Users\\admin123\\Desktop\\test.html"), "utf8");
+        Document doc = Jsoup.parse(content);
+        String title = doc.getElementsByTag("title").first().text();
+        System.out.println(title);
     }
 }
